448aba33111c64ae28cee4000544e3dd93b4eb59
Alex Wernick <alex.j.wernick@gmail.com>
2024-09-18 08:04:37 +0100

    small tidy

--- kaggle_push.ps1
+++ kaggle_push.ps1
@@ -1,6 +1,5 @@
 Get-ChildItem -Path ".\kaggle_data\arc_2024\" | Remove-Item -Recurse -Force
 Copy-Item -Path ".\arc_2024\data_management\" -Destination ".\kaggle_data\arc_2024\" -Recurse -Force
 Copy-Item -Path ".\arc_2024\.env.kaggle" -Destination ".\kaggle_data\arc_2024\.env" -Force
-#Copy-Item -Path ".\arc_2024\__init__.py" -Destination ".\kaggle_data\arc_2024\__init__.py" -Force
 kaggle datasets version -p kaggle_data/ -m "Updated data" --dir-mode tar
 kaggle kernels push -p arc_2024/
